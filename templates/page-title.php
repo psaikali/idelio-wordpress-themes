1c7ca240cee938e4cd5a098eae6a5e71e42a228c
Pierre Saïkali <dev@mosaika.fr>
2018-03-29 17:08:29 +0200

    Ajout breadcrumbs

--- templates/page-title.php
+++ templates/page-title.php
@@ -1,4 +1,7 @@
 <section id="page-title">
+	<?php if (function_exists('yoast_breadcrumb')) {
+		yoast_breadcrumb('<nav class="breadcrumbs">', '</nav>');
+	} ?>
 	<?php ay_page_title(); ?>
 	<?php ay_page_subtitle(); ?>
 </section>
