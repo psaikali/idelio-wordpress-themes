944786d9869db8cf38354b9fb270af8f0ab659b2
Pierre Saïkali <dev@mosaika.fr>
2018-03-30 16:19:16 +0200

    CSS hero homepage

--- templates/home-hero.php
+++ templates/home-hero.php
@@ -4,6 +4,7 @@ $background = get_field('background');
 $illustration = get_field('illustration');
 $main_text = get_field('main_text');
 $promo_text = get_field('promo_text');
+$has_main_button = false;
 
 ?>
 
@@ -18,13 +19,18 @@ $promo_text = get_field('promo_text');
 						the_row();
 						if (have_rows('main_button')) {
 							while (have_rows('main_button')) {
+								$has_main_button = true;
 								the_row(); ?>
 								<a href="<?php echo esc_url(get_sub_field('link')); ?>" class="button plain first"><?php the_sub_field('label'); ?></a>
 							<?php }
 						}
+						
 						if (have_rows('secondary_link')) {
 							while (have_rows('secondary_link')) {
-								the_row(); ?>
+								the_row(); 
+								if ($has_main_button) { ?>
+								<span class="or"><?php _e('ou', 'ayiha'); ?></span>
+								<?php } ?>
 								<a href="<?php echo esc_url(get_sub_field('link')); ?>" class="link second"><?php the_sub_field('label'); ?></a>
 							<?php }
 						}
@@ -39,10 +45,12 @@ $promo_text = get_field('promo_text');
 		</figure>
 		<?php } ?>
 		
-		<?php if (strlen(trim($promo_text)) > 0) { ?>
 		<aside class="promo-text">
-			<?php echo wpautop(do_shortcode($promo_text)); ?>
+			<div class="mask mask-left"></div>
+			<div class="mask mask-right"></div>
+			<?php if (strlen(trim($promo_text)) > 0) {
+				echo wpautop(do_shortcode($promo_text));
+			} ?>
 		</aside>	
-		<?php } ?>
 	</div>
 </section>
