fa09d62901756168f195488c88bfa006e10d9d11
Pierre Saïkali <dev@mosaika.fr>
2018-04-04 11:06:58 +0200

    ACF blocs de contenu : stylisation CSS admin

--- lib/ayiha/hooks.php
+++ lib/ayiha/hooks.php
@@ -106,3 +106,133 @@ function ay_custom_code_css_in_head() {
 	);
 }
 add_action('wp_head', 'ay_custom_code_css_in_head');
+
+
+/**
+ * Admin CSS for more understandable flexible content
+ */
+function ay_custom_css_in_admin() { ?>
+<style>
+.acf-fc-popup{
+	width:80%;
+	max-width:1200px;
+	height:80%;
+	max-height:1200px;
+	top: 10% !important;
+	left: 10% !important;
+	bottom: 10% !important;
+	right: 10% !important;
+	border-radius:0;
+	position: fixed !important;
+	background: none !important;
+	padding:0;
+	box-sizing:border-box;
+}
+.acf-fc-popup *{
+	box-sizing:border-box;
+}
+.acf-fc-popup::before, .acf-fc-popup::after {
+	border-color:transparent !important;
+}
+.acf-fc-popup:before{
+	top: -100% !important;
+	right: 0 !important;
+	bottom: 0 !important;
+	left: -100% !important;
+	position: fixed;
+	background: rgba(0,0,0,0.8);
+	height: 200vh;
+	width: 200vw;
+	margin: 0 !important;
+	padding: 0;
+	content: "";
+}
+.acf-fc-popup ul{
+	overflow:auto;
+	height:100%;
+	width:100%;
+	background:#f4f4f4;
+	border:#ccc;
+	padding:15px;
+	position:relative;
+	box-shadow:0 0 8px rgba(0, 0, 0, 0.5);
+	padding-top: 100px !important;
+}
+.acf-fc-popup ul:before{
+	position: absolute;
+	top: 31px;
+	content: "Ajouter un bloc de contenu";
+	font-size: 23px;
+	font-weight: 400;
+	color: #333;
+	left: 30px;
+}
+.acf-fc-popup ul:after{
+	position: absolute;
+	top: 62px;
+	content: "Selectionnez le type de bloc de contenu à ajouter sur la page.";
+	font-size: 14px;
+	color: #666;
+	left: 30px;
+	font-style: italic;
+}
+.acf-fc-popup ul li{
+	width: 33.33333%;
+	padding: 1%;
+	float: left;
+}
+@media only screen and (max-width:960px){
+	.acf-fc-popup ul li{
+		width: 50%;
+    }
+}
+@media only screen and (max-width:782px){
+	.acf-fc-popup{
+		width:95%;
+		height:88%;
+	}
+	.acf-fc-popup ul li{
+		width: 100%;
+    }
+}
+.acf-fc-popup ul li a {
+	border:3px solid #ddd;
+	background:#fff;
+	padding:15px;
+	color:#333;
+	text-align: center;
+	text-transform: uppercase;
+}
+.acf-fc-popup ul li a:hover {
+	border-color:#fff;
+}
+.acf-fc-popup ul li a[data-layout]:before {
+	content:"";
+	display: block;
+	height:0;
+	width:100%;
+	padding-bottom:50%;
+	background: #333;
+	position: relative;
+	margin-bottom: .5rem;
+	background-size: cover !important;
+	border:1px solid #ddd;
+}
+.acf-fc-popup ul li a[data-layout=simple_content]:before {
+	background:url('<?php echo get_template_directory_uri(); ?>/dist/images/admin-content-block-simple-content.jpg') center center;
+}
+.acf-fc-popup ul li a[data-layout=text_and_cards]:before {
+	background:url('<?php echo get_template_directory_uri(); ?>/dist/images/admin-content-block-text-cards.jpg') center center;
+}
+.acf-fc-popup ul li a[data-layout=featured_pages]:before {
+	background:url('<?php echo get_template_directory_uri(); ?>/dist/images/admin-content-block-featured-pages.jpg') center center;
+}
+.acf-fc-popup ul li a[data-layout=pricing_table]:before {
+	background:url('<?php echo get_template_directory_uri(); ?>/dist/images/admin-content-block-pricing-table.jpg') center center;
+}
+.acf-fc-popup ul li a[data-layout=image_and_text_slider]:before {
+	background:url('<?php echo get_template_directory_uri(); ?>/dist/images/admin-content-block-slider.jpg') center center;
+}
+</style>
+<?php }
+add_action('admin_head', 'ay_custom_css_in_admin');
